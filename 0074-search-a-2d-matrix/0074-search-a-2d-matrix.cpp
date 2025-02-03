9c0607b5d3edc2c02e90f54137db0777a377e526
Aarush Jain <36598074+aarushjain355@users.noreply.github.com>
2025-02-03 11:14:55 -0500

    Time: 0 ms (100%), Space: 13.7 MB (7.6%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -10,20 +10,7 @@ public:
         int low = 0;
         int high = arr.size();
         int mid = ((high + low) / 2);
-        if (arr.size() == 2) {
-            if (arr[0] == target || arr[1] == target) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-         if (arr.size() == 1) {
-            if (arr[0] == target) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+        
         while (true) {
             if (arr[mid] < target) {
                 if (mid == high || mid == low) {                   
