a0ea1681f750126e94084e06a1b79d63174dc311
Aarush Jain <36598074+aarushjain355@users.noreply.github.com>
2025-03-15 21:30:10 -0400

    Time: 20 ms (35.58%), Space: 12.5 MB (53.13%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        sum1 = 0
+        sum2 = 0
+        sum3 = 0
+        for x in range(len(num1)):
+            print(sum1)
+            sum1 = sum1 * 10 + int(num1[x])
+        for x in range(len(num2)):
+            sum2 = sum2 * 10 + int(num2[x])
+        print(sum1)
+        print(sum2)
+        sum3 = sum1 * sum2  
+        return str(sum3)
+
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        
